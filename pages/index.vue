674ca37835fc696459a2dae40412cf78e11c62e3
ydah <t.yudai92@gmail.com>
2024-04-05 01:34:46 +0900

    Add LP

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,28 @@
 <template>
-  <v-app-bar :elevation="2">
-    <v-app-bar-title>Osaka RubyKaigi 2024</v-app-bar-title>
-  </v-app-bar>
+  <div class="fill-height">
+    <v-row align-content="center fill-height">
+      <v-col align="center">
+        <v-card
+          :variant="flat"
+          min-width="100vw"
+          class="text-center py-10"
+        >
+          <v-card-item class="my-10">
+            <div>
+              <div class="text-h1 mb-7">
+                Osaka RubyKaigi 04
+              </div>
+              <div class="text-h2 mb-15">
+                2024-08
+              </div>
+              <div class="text-h4 mb-10">comming soon...</div>
+            </div>
+            <v-btn href="https://twitter.com/rubykansai">
+              <v-icon icon="mdi-twitter" class="text-h4"></v-icon>
+            </v-btn>
+          </v-card-item>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
 </template>
