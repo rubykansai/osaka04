8eb3ab0b3e647a4d18f79336b23bf92d3829e322
ydah <t.yudai92@gmail.com>
2024-07-21 18:42:10 +0900

    Update sponsors page with new sponsor information

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -10,7 +10,7 @@
         <v-btn>Speakers</v-btn>
         -->
         <NuxtLink class="d-none d-sm-block" to="/"><v-btn>Home</v-btn></NuxtLink>
-        <!-- <NuxtLink class="d-none d-sm-block" to="/sponsors"><v-btn>Sponsors</v-btn></NuxtLink> -->
+        <NuxtLink class="d-none d-sm-block" to="/sponsors"><v-btn>Sponsors</v-btn></NuxtLink>
         <NuxtLink class="d-none d-sm-block" to="/venue"><v-btn>Venue</v-btn></NuxtLink>
         <NuxtLink class="d-none d-sm-block" to="/policies"><v-btn>Policies</v-btn></NuxtLink>
         <NuxtLink class="d-none d-sm-block" to="/about"><v-btn>About</v-btn></NuxtLink>
@@ -20,7 +20,7 @@
     <v-navigation-drawer v-model="drawer">
       <v-list>
         <NuxtLink to="/"><v-list-item>Home</v-list-item></NuxtLink>
-        <!-- <NuxtLink to="/sponsors"><v-list-item>Sponsors</v-list-item></NuxtLink> -->
+        <NuxtLink to="/sponsors"><v-list-item>Sponsors</v-list-item></NuxtLink>
         <NuxtLink to="/venue"><v-list-item>Venue</v-list-item></NuxtLink>
         <NuxtLink to="/policies"><v-list-item>Policies</v-list-item></NuxtLink>
         <NuxtLink to="/about"><v-list-item>About</v-list-item></NuxtLink>
